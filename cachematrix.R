52fd0c90ee61d4c194e6f04d8c3b1eb88713e6e1
egalion <egalion@yahoo.com>
2015-06-22 00:34:02 +0300

    My submission for the R Programming course with added comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,30 +3,51 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+	  m <- NULL   # remove m
+  # function to set y to x in the parent environment
+  set <- function(y) {  
+	      x <<- y
+      m <<- NULL
+        }
+    get <- function() x   # function to get the matrix value
+    # set another value for the matrix in the parent environment
+    setinverse <- function(inverse) m <<- inverse 
+      getinverse <- function() m  # get the matrix of the value
+      # create a list with the functions
+      list(set = set, get = get,
+	          setinverse = setinverse,
+		         getinverse = getinverse)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
+	  ## Return a matrix that is the inverse of 'x'
+	  # get the matrix, from the makeCacheMatrix function
+	  # if the inverse exists, print a message and return the value of the matrix
+	  m <- x$getinverse()
   if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+	      message("getting cached data")
+      return(m)
+        }
+    # if not, get the original matrix from makeCacheMatrix
+    # and calculate its inverse; output the inverse.
+    data <- x$get()
+    m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
+
+# test with
+# m <- matrix(c(-1, -2, 1, 1), 2,2)
+# x <- makeCacheMatrix(m)
+# inv <- cacheSolve(x)
+# m %*% inv
+
+
+
+
+
+
+
+
